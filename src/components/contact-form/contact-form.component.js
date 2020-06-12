b0549f09b86feb0fd05ddbff1872357f5b695ae7
Max Herman <maxherman@Maxs-MBP.lan>
2020-06-12 15:01:56 -0400

    made form clear after submit

--- src/components/contact-form/contact-form.component.js
+++ src/components/contact-form/contact-form.component.js
@@ -1,28 +1,71 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 
-class ContactForm extends React.Component{
+import CustomButton from '../custom-button/custom-button.component'
 
-    sendEmail(e) {
-        e.preventDefault();
-        /* emailjs.sendForm('gmail', 'template_SYvZvBnN', e.target, 'user_U8u5G9v2HcZ6gyV8weBYB')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        }); */  
+
+class ContactForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            from_name: '',
+            user_email: '',
+            message_html: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.sendEmail = this.sendEmail.bind(this)
     }
+
+    sendEmail = e =>  {
+        e.preventDefault();
+
+        try{
+            emailjs.sendForm('gmail', 'template_SYvZvBnN', e.target, 'user_U8u5G9v2HcZ6gyV8weBYB');
+            this.setState({
+                from_name: '',
+                user_email: '',
+                message_html: ''
+            })
+            alert('rob is megagay')
+        }catch(error){
+            alert('There was an unknown error. Please try again later.')
+        }
+    }
+
+    handleChange = event => {
+        const  {value, name} = event.target;
+        this.setState({[name]:value})
+    }
+
     render(){
         return (
           <form className="contact-form" onSubmit={this.sendEmail}>
             <input type="hidden" name="contact_number" required/>
             <label >Name</label>
-            <input type="text" name="from_name" required/>
+            <input 
+                type="text"
+                name="from_name"
+                value={this.state.from_name}
+                onChange={this.handleChange}
+                required
+            />
             <label>Email</label>
-            <input type="email" name="user_email" required/>
+            <input
+                type="email"
+                name="user_email"
+                value={this.state.user_email}
+                onChange={this.handleChange}
+                required
+            />
             <label>Message</label>
-            <textarea name="message_html" />
-            <input type="submit" value="Send" />
+            <textarea
+                name="message_html"
+                value={this.state.message_html}
+                onChange={this.handleChange}
+                required
+            />
+            <CustomButton inverted type='submit'>SUBMIT</CustomButton>
           </form>
         );
     }
