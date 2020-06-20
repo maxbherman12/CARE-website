d52650f1de2c6ea9d4f2aec47f279c9aff179f46
Max Herman <maxherman@Maxs-MBP.lan>
2020-06-20 15:11:19 -0400

    started styling the homepage

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -3,10 +3,12 @@ import './home.styles.css';
 
 const HomePage = () => (
     <div className="home">
-        <h1>HOMEPAGE</h1>
-        <h2>Our Mission</h2>
-        <h2>Event</h2>
-        <h2>Donate Now</h2>
+        <div className="img-container">
+            <div className="title-container">
+                <h1>Westport Stands for Justice:<br />A Concert for #BlackLivesMatter</h1>
+            </div>
+            <img src="https://images.unsplash.com/photo-1590945796812-e577d2d0f73d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" alt=""/>
+        </div>
     </div>
 )
 
