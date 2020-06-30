b87d14bf9afb97dc7d2470c531f7262ee7179dc6
Max Herman <maxherman@Maxs-MBP.lan>
2020-06-29 21:25:09 -0400

    changed logo img

--- src/pages/donate/donate.page.js
+++ src/pages/donate/donate.page.js
@@ -9,7 +9,7 @@ const DonatePage = () => (
          <div className="button-container">
             <CustomButton onClick={() => alert('We are currently working on setting up our donation management system. Please check back again in a few days. We apologize for the inconvenience.')} border>DONATE to CARE.</CustomButton>
         </div>
-        <h1>To donate to these hardworking, local organizations, please click the link above.</h1>
+        <h2>To donate to these hardworking, local organizations, please click the link above. Scroll through our gallery to learn more.</h2>
         <Gallery />
     </div>
 )
